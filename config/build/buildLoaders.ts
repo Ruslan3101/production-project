749f844e189a71274fcb6d6d971b4cacc25688ab
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-09-19 20:37:34 -0700

    build: init react and setup scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,23 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-  const typescriptLoader = {
-    test: /\.tsx?$/,
-    use: "ts-loader",
-    exclude: /node_modules/,
+    const cssLoaders = {
+        test: /\.s[ac]ss$/i,
+        use: [
+        // Creates `style` nodes from JS strings
+        "style-loader",
+        // Translates CSS into CommonJS
+        "css-loader",
+        // Compiles Sass to CSS
+        "sass-loader",
+        ],
   };
+  //if we are not using ts then we have to use babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+    };
 
-  return [typescriptLoader];
+  return [typescriptLoader, cssLoaders];
 }
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+
+render(
+    <div>Hello App</div>,
+    document.getElementById("root")
+);
