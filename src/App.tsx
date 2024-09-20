3cf3823f25947dda5cfed7b2b95a0c7d3161ce7d
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-09-20 15:54:36 -0700

    build: implement react router dome and lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import './index.scss'
+import "./index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
+
 const App = () => {
-    return (
-        <div className='app'>
-            Hello App
-        </div>
-    );
+  return (
+    <div className="app">
+      <Link to={"/"}>Main Page</Link>
+      <Link to={"/about"}>About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
