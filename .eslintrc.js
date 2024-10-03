fca603c70fa89faf2a981a5428e4b907bbc7ae78
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-10-02 17:54:10 -0700

    fix: fix eslint max-len

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,7 +39,8 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['to'] }],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
+        'max-len': ['error', { ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
