51ebc5f23110414558b60e7145cbb71575eac634
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-10-27 12:49:18 -0700

    test: add react testign libraty, add test for btn, sidebar

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -2,7 +2,6 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
 import { useTranslation } from 'react-i18next';
 
-
 interface LangSwitcherProps {
   className?: string;
 }
