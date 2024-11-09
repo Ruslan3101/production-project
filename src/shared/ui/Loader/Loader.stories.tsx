6482adb3c8d2c333fa70a4dbcf853f9662b7728e
Rusalan Lutfullin <ruslanlutfullin95@gmail.com>
2024-11-08 20:16:07 -0800

    test: add storybook, loki screenshot tests

--- src/shared/ui/Loader/Loader.stories.tsx
+++ src/shared/ui/Loader/Loader.stories.tsx
@@ -3,13 +3,13 @@ import { fn } from '@storybook/test';
 import 'app/styles/index.scss';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
-import { Loader, ThemeButton } from './Loader';
+import { Loader } from './Loader';
 
 const meta = {
     title: 'shared/Loader',
     component: Loader,
 
-    args: { onClick: fn() },
+    args: {},
 } satisfies Meta<typeof Loader>;
 
 export default meta;
